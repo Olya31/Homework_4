6331b6604b13c885afd680b22029cd8373d72c58
Olya31 <79367798+Olya31@users.noreply.github.com>
2021-02-28 21:08:33 +0300

    For check

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter n:");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] array = new int[N];
+            Random numbers = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = numbers.Next(0, N);
+            }
+
+            int numberToDivided = 0;
+
+            foreach (int numberInArray in array)
+            {
+                if (numberInArray > 1)
+                {
+                    numberToDivided = numberInArray;
+                    bool flagChoice = true;
+                    for (int i = 2; i < numberToDivided; i++)
+                    {
+                        if (numberToDivided % i == 0)
+                        {
+                            flagChoice = false;
+                            break;
+                        }
+
+                    }
+                    if (flagChoice)
+                    {
+                        Console.WriteLine("Prime number: {0}", numberToDivided);
+                    }
+                }
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
